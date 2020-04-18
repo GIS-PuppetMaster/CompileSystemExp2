1e262fb2abbbb5a294c075186892f277c9f5ea18
zkx741481546 <zkx741481546@live.com>
2020-04-18 17:47:22 +0800

    初始状态为0号

--- src/zkx/AnalysisFormat.java
+++ src/zkx/AnalysisFormat.java
@@ -4,9 +4,9 @@ package zkx;
 import Action.Action;
 import Action.Express;
 
+import javax.print.attribute.HashAttributeSet;
 import java.sql.ClientInfoStatus;
 import java.util.*;
-
 
 
 public class AnalysisFormat {
@@ -16,12 +16,12 @@ public class AnalysisFormat {
         this.action = action;
     }
 
-    public Set<String> getFinalSymbolSet(){
+    public Set<String> getFinalSymbolSet() {
         Set<String> characterSet = new HashSet<>();
         Set<String> notFinalSymbol = action.getVirSet();
         for (Express express : action.getProList()) {
-            for(String symbol:express.getRight()){
-                if(!notFinalSymbol.contains(symbol)){
+            for (String symbol : express.getRight()) {
+                if (!notFinalSymbol.contains(symbol)) {
                     characterSet.add(symbol);
                 }
             }
@@ -29,6 +29,7 @@ public class AnalysisFormat {
         characterSet.add("$");
         return characterSet;
     }
+
     public Set<String> getSymbolSet() {
         Set<String> notFinalSymbol = action.getVirSet();
         Set<String> finalSymbol = getFinalSymbolSet();
@@ -38,22 +39,23 @@ public class AnalysisFormat {
         return characterSet;
     }
 
-    public Set<Set<Express>> items(List<Express> G) {
+    public List<Object> items(List<Express> G) {
         Express start = G.get(0);
         start.setHopingSymbols("$");
         boolean added = false;
+        Set<Express> firstState = action.closure_method(new HashSet<>() {{
+            add(start);
+        }});
         Set<Set<Express>> C = new HashSet<>() {{
-            add(action.closure_method(new HashSet<>() {{
-                add(start);
-            }}));
+            add(firstState);
         }};
         ArrayList<Set<Express>> arrayList = new ArrayList<>(C);
         int size;
         do {
-            added=false;
-            for(int i=0;;i++){
+            added = false;
+            for (int i = 0; ; i++) {
                 size = arrayList.size();
-                if(i>=size){
+                if (i >= size) {
                     break;
                 }
                 Set<Express> set = arrayList.get(i);
@@ -67,17 +69,29 @@ public class AnalysisFormat {
                 }
             }
         } while (added);
-        return new HashSet<>(arrayList);
+        Set<Set<Express>> res = new HashSet<>(arrayList);
+        return new ArrayList<>() {{
+            add(firstState);
+            add(res);
+        }};
     }
 
     public List<Object> buildFormat() {
         Map<Integer, Map<String, FormatElement>> format = new HashMap<>();
-        Set<Set<Express>> C = items(action.getProList());
+        List<Object> list = items(action.getProList());
+        Set<Express> firstState = (Set<Express>) list.get(0);
+        Set<Set<Express>> C = (Set<Set<Express>>) list.get(1);
         Map<Integer, Set<Express>> indexMap = new HashMap<>();
         Map<Set<Express>, Integer> inverseIndexMap = new HashMap<>();
-        int indexCode = 0;
+        indexMap.put(0, firstState);
+        inverseIndexMap.put(firstState, 0);
+        format.put(0, new HashMap<>());
+        int indexCode = 1;
         for (Set<Express> expresses : C) {
             //对状态进行编码
+            if(expresses==firstState){
+                continue;
+            }
             indexMap.put(indexCode, expresses);
             inverseIndexMap.put(expresses, indexCode);
             format.put(indexCode, new HashMap<>());
@@ -108,8 +122,7 @@ public class AnalysisFormat {
                         // 获取这个状态的编号
                         int j = inverseIndexMap.get(exp);
                         format.get(inverseIndexMap.get(expresses)).put(next, new FormatElement(j, "s"));
-                    }
-                    else {
+                    } else {
                         Set<Express> exp = action.goto_method(expresses, next);
                         // 获取这个状态的编号
                         int j = inverseIndexMap.get(exp);
@@ -134,52 +147,50 @@ public class AnalysisFormat {
         // 文法符号-终结符=非终结符
         allSymbol.removeAll(finalSymbol);
         List<String> notFinalSymbol = new ArrayList<>(allSymbol);
-        for(int i = 0;i<finalSymbol.size()*10-7;i++) {
-            firstHead.insert(13," ");
+        for (int i = 0; i < finalSymbol.size() * 10 - 7; i++) {
+            firstHead.insert(13, " ");
         }
 
         System.out.println(firstHead);
         // 构造表头format
         String formatHead = "    ";
-        for(int i = 0;i<finalSymbol.size();i++){
-            formatHead+="|{%5s}";
+        for (int i = 0; i < finalSymbol.size(); i++) {
+            formatHead += "|{%5s}";
         }
-        formatHead+="||";
-        for(int i = 0;i<notFinalSymbol.size();i++){
-            formatHead+="|{%5s}";
+        formatHead += "||";
+        for (int i = 0; i < notFinalSymbol.size(); i++) {
+            formatHead += "|{%5s}";
         }
         // 输出表头
         StringBuilder line = new StringBuilder("     ||");
-        for(String s:finalSymbol){
-            line.append(String.format("%9s",s)).append("|");
+        for (String s : finalSymbol) {
+            line.append(String.format("%9s", s)).append("|");
         }
         line.append("|");
-        for(String s:notFinalSymbol){
-            line.append(String.format("%9s",s)).append("|");
+        for (String s : notFinalSymbol) {
+            line.append(String.format("%9s", s)).append("|");
         }
         System.out.println(line);
         // 遍历状态
         for (int state = 0; state < format.keySet().size(); state++) {
             line = new StringBuilder();
-            line.append(String.format("%5s||",state));
+            line.append(String.format("%5s||", state));
             // 遍历Action的终结符
-            for(String s:finalSymbol){
+            for (String s : finalSymbol) {
                 FormatElement formatElement = format.get(state).get(s);
-                if(formatElement==null){
-                    line.append(String.format("%9s|",""));
-                }
-                else{
+                if (formatElement == null) {
+                    line.append(String.format("%9s|", ""));
+                } else {
                     line.append(String.format("%9s|", formatElement));
                 }
             }
             line.append("|");
             // 遍历GOTO的非终结符
-            for(String s:notFinalSymbol){
+            for (String s : notFinalSymbol) {
                 FormatElement formatElement = format.get(state).get(s);
-                if(formatElement==null){
-                    line.append(String.format("%9s|",""));
-                }
-                else{
+                if (formatElement == null) {
+                    line.append(String.format("%9s|", ""));
+                } else {
                     line.append(String.format("%9s|", formatElement));
                 }
             }
